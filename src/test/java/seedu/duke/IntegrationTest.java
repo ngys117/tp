0c14ba029c2725395b4026d14df6ef18f7dcad02
ngys117 <e0540880@u.nus.edu>
2022-04-03 19:51:50 +0900

    Refactor Code after file rename

--- src/test/java/seedu/duke/IntegrationTest.java
+++ src/test/java/seedu/duke/IntegrationTest.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.fail;
 
-public class UnitTest {
+public class IntegrationTest {
 
     static class TestSystem {
         private ModuleList moduleList = new ModuleList();
